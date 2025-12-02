f13895647015fd7dee139b7f1b8c4010eb5512f2
keshala <senuu08@gmail.com>
2025-12-02 09:43:53 +0530

    Add LoginPage component with authentication form

---
+++ app/login/page.jsx
@@ -0,0 +1,9 @@
+import LoginForm from "./login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center text-black rounded">
+      <login-form />
+    </div>
+  );
+}
